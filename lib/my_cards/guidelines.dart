1a652709d5540b32af5902e0891f9c6302b19b02
Marcel <marcel.garus@gmail.com>
2018-09-27 16:27:50 +0200

    Overhauled guidelines with examples.

--- lib/my_cards/guidelines.dart
+++ lib/my_cards/guidelines.dart
@@ -2,19 +2,23 @@ import 'package:flutter/material.dart';
 
 /// An item in the guidelines list.
 class GuidelineItem {
-  GuidelineItem({ this.icon, this.title, this.content });
+  GuidelineItem({
+    this.title,
+    this.content,
+    this.positiveExample = '',
+    this.negativeExample = '',
+  });
 
-  final Icon icon;
   final String title;
   final String content;
+  final String positiveExample;
+  final String negativeExample;
   bool isExpanded = false;
 
   Widget get header {
     return Row(
       children: <Widget>[
         SizedBox(width: 16.0),
-        icon,
-        SizedBox(width: 8.0),
         Expanded(child: Text(title))
       ],
     );
@@ -23,7 +27,32 @@ class GuidelineItem {
   Widget get body {
     return Container(
       padding: EdgeInsets.only(left: 16.0, right: 16.0, bottom: 16.0),
-      child: Text(content)
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(content),
+          SizedBox(height: 8.0),
+          Row(
+            children: <Widget>[
+              _buildCard(positiveExample, Colors.green),
+              SizedBox(width: 8.0),
+              _buildCard(negativeExample, Colors.red)
+            ],
+          )
+        ]
+      )
+    );
+  }
+
+  Widget _buildCard(String text, Color color) {
+    return Material(
+      elevation: 2.0,
+      borderRadius: BorderRadius.circular(4.0),
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(8.0),
+        child: Text(text, style: TextStyle(color: color))
+      ),
     );
   }
 }
@@ -37,16 +66,33 @@ class Guidelines extends StatefulWidget {
 class _GuidelinesState extends State<Guidelines> {
   final guidelines = [
     GuidelineItem(
-      icon: Icon(Icons.people_outline),
       title: 'How to include players',
-      content: 'You can use Alice and Bob as placeholders for names. During '
-          'the game, these will be replaced by actual names.'
+      content: 'You can use \$a, \$b etc. as placeholders for names. During '
+        'the game, these will be replaced by actual names.',
+      positiveExample: '\$a, mach etwas.',
+      negativeExample: 'Alice, mach etwas.'
     ),
     GuidelineItem(
-      icon: Icon(Icons.description),
-      title: 'Guidelines',
-      content: 'Write numbers as digits (except one)\nNew lined text.'
-    )
+      title: 'First person speech',
+      content: 'Try to use first person speech whenever possible in order to '
+        'be more engaging.',
+      positiveExample: '\$a, tu etwas.',
+      negativeExample: '\$a muss etwas tun.'
+    ),
+    GuidelineItem(
+      title: 'Punctuation',
+      content: 'Even in imperative sentences, use a period instead of an '
+        'exclamation mark. Otherwise, players would feel like being shouted '
+        'at all the time.',
+      positiveExample: '\$a, mach etwas.',
+      negativeExample: '\$a mach etwas!'
+    ),
+    GuidelineItem(
+      title: 'Numbers',
+      content: 'Write all numbers except one as digits.',
+      positiveExample: '\$a, tu 3 Sachen.',
+      negativeExample: '\$a tu drei Sachen.'
+    ),
   ];
 
   void expansionCallback(int index, bool isExpanded) {
--- lib/my_cards/publish_card.dart
+++ lib/my_cards/publish_card.dart
@@ -25,60 +25,72 @@ class PublishCardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final content = <Widget>[];
-    print('Building publish card $card.');
 
-    content.add(Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Text(
-        'Review your card.',
-        style: TextStyle(fontSize: 24.0),
-      ),
-    ));
-
-    content.add(Padding(
-      padding: EdgeInsets.all(16.0),
-      child: InlineCard(card.gameCard, showFollowup: false)
-    ));
+    content.addAll([
+      Text('Review your card.', style: TextStyle(fontSize: 24.0)),
+      SizedBox(height: 16.0),
+      InlineCard(card.gameCard, showFollowup: false)
+    ]);
 
     if (card.gameCard.hasFollowup) {
-      content.add(Center(
-        child: Material(
-          color: Colors.white,
-          elevation: 2.0,
-          shape: StadiumBorder(),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
-            child: Text('Then, after some time:')
+      content.addAll([
+        SizedBox(height: 8.0),
+        Center(
+          child: Material(
+            color: Colors.white,
+            elevation: 2.0,
+            shape: StadiumBorder(),
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+              child: Text('Then, after some time:')
+            )
           )
-        )
-      ));
-      content.add(Padding(
-        padding: EdgeInsets.all(16.0),
-        child: InlineCard(card.gameCard.createFollowup())
-      ));
+        ),
+        SizedBox(height: 8.0),
+        InlineCard(card.gameCard.createFollowup())
+      ]);
     }
 
-    content.add(Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Guidelines()
-    ));
-
-    content.add(Container(
-      padding: EdgeInsets.only(bottom: 16.0),
-      alignment: Alignment.center,
-      child: FloatingActionButton.extended(
-        icon: Icon(Icons.cloud_upload),
-        label: Text('Publish'),
-        elevation: 12.0,
-        onPressed: _publish,
+    content.addAll([
+      SizedBox(height: 16.0),
+      Guidelines(),
+      SizedBox(height: 16.0),
+      Text(
+        'Once you click the publish button, you agree to the following '
+        'process: '
+        'The card\'s "input" (content, followup and author) as well as your '
+        'email address will be uploaded to and permanently stored on '
+        'Google\'s servers. By publishing the card, you automatically '
+        'transfer the input\'s copyright to this app\'s creator Marcel Garus, '
+        'who will review it manually and - to his liking - add it to the '
+        'database of cards. If he has any questions, he may mail you for '
+        'clarification. '
+        'You confirm that the input adheres to the guidelines stated above. '
+        'After publishing the card, you will not be able to edit or delete '
+        'the card any more.',
+        style: TextStyle(fontSize: 10.0),
+        textAlign: TextAlign.justify,
+      ),
+      SizedBox(height: 16.0),
+      Center(
+        child: FloatingActionButton.extended(
+          icon: Icon(Icons.cloud_upload),
+          label: Text('Publish'),
+          onPressed: _publish,
+        )
       )
-    ));
+    ]);
 
     return Theme(
       data: Utils.myCardsTheme,
       child: Scaffold(
         appBar: AppBar(title: Text('Publish card')),
-        body: SafeArea(child: ListView(children: content))
+        body: SafeArea(
+          child: ListView(
+            padding: EdgeInsets.all(16.0),
+            children: content
+          )
+        )
       ),
     );
   }
